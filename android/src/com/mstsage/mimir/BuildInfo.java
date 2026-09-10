b044e5b4f216af740e596d1e2454f6b083d6e933
Rane <rane@mstsage.com>
2026-09-10 04:10:30 -0400

    Donate link (configurable via mimir.config.json, hidden in --play builds), source link and licenses on About; BuildInfo generated from config

--- android/src/com/mstsage/mimir/BuildInfo.java
+++
@@ -1,7 +0,0 @@
-package com.mstsage.mimir;
-
-/** Version constants kept in sync with AndroidManifest.xml by bin/build.rb. */
-final class BuildInfo {
-    static final String VERSION = "0.6.0";
-    private BuildInfo() {}
-}
