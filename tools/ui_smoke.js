064d3d3ecd5cb2f30ca6b4070986f9406c248bf6
Rane <rane@mstsage.com>
2026-09-10 03:08:13 -0400

    ui_smoke: resolve relative bundle path

--- tools/ui_smoke.js
+++ tools/ui_smoke.js
@@ -5,7 +5,7 @@ global.document = { activeElement:null, getElementById(id){ return els[id] ||= m
 global.window = global; global.host = { send(j){ console.log('host.send', j.slice(0,120)); } };
 global.setTimeout = (f)=>f();
 try {
-  require(process.argv[2]);
+  require(require('path').resolve(process.argv[2]));
   if (!(els.tabs && els.tabs.innerHTML.length)) throw new Error('tabs did not render');
   const st = {"tabs":[{"id":1,"url":"https://a.test/","title":"A","progress":100,"loading":false,"favicon":null}],"current":1,"url":"https://a.test/","progress":100,"loading":false,"desktop":true,"can_back":false,"can_forward":false,"bookmarks":[],"history":[],"bookmarks_bar":true,"devtools":{"open":false,"side":"right","fraction":0.45}};
   global.UI.receive(JSON.stringify(st));
